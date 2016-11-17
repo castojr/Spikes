d95b2e61b905c5f5675195c109bfd974686e7fe9
Joey Casto <castojr@yahoo.com>
2016-11-17 11:02:26 -0800

    edited JoeysHackathon

--- SupportCenterKnowledgeBase/SupportCenterKnowledgeBase/JoeysHackathon.aspx.cs
+++ SupportCenterKnowledgeBase/SupportCenterKnowledgeBase/JoeysHackathon.aspx.cs
@@ -30,14 +30,14 @@ namespace SupportCenterKnowledgeBase
 
         private void Temp()
         {
-            /*var multiPassGenerator = new MultipassGenerator(ConfigurationManager.AppSettings["siteName"], ConfigurationManager.AppSettings["apiKey"]);
-            string url = multiPassGenerator.GenerateLine(tbName.Text, tbEmail.Text, tbLevel.Text, ddlBrand.SelectedValue);
-            Response.Redirect(url);*/
+            var multiPassGenerator = new MultipassGenerator(ConfigurationManager.AppSettings["siteName"], ConfigurationManager.AppSettings["apiKey"]);
+            string url = multiPassGenerator.GenerateLine(Session["userType"].ToString(), Session["userType"].ToString() + "@example.com", Session["userType"].ToString(), ConfigurationManager.AppSettings[Session["userType"].ToString()]);
+            Response.Redirect(url);
         }
 
         protected void brandLinkButton_Click(object sender, EventArgs e)
         {
-
+            Temp();
         }
     }
 }
